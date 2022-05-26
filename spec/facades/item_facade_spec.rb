cd67f37f826f4948205cfdf8917bc14da703352c
Scott Sullivan <scottsullivanltd@gmail.com>
2022-05-26 03:00:16 -0600

    Refactor: missing an 's' bug, fixed

--- spec/facades/item_facade_spec.rb
+++ spec/facades/item_facade_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe ItemFacade do
   it "creates item poros", :vcr do
-    item = ItemFacade.create_item
+    item = ItemFacade.create_items
     expect(item).to be_an_instance_of(Item)
   end
 end
--- spec/facades/merchant_facade_spec.rb
+++ spec/facades/merchant_facade_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe MerchantFacade do
   it "creates merchant poros", :vcr do
-    merch = MerchantFacade.create_merchant
+    merch = MerchantFacade.create_merchants
     expect(merch).to be_an_instance_of(Merchant)
   end
 end
